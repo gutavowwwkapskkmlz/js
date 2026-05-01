e76d4c8a969dbf81e3011f78cfe4960af68f4e96
GUSTAVO SANTOS OLIVEIRA <gustavoesantos13@gmail.com>
2026-05-01 15:30:59 -0300

    data final

--- data.js
+++ data.js
@@ -7,17 +7,40 @@ let momentoAtual = new Date();
 
 // 2. Usamos as ferramentas "get" (pegar) para fatiar a foto
 let dia = momentoAtual.getDate();
-let mes = momentoAtual.getMonth(); // Lembra do perigo aqui!
+let mes = dataFormatura.getMonth() +1; // Lembra do perigo aqui!
 let ano = momentoAtual.getFullYear();
 
 console.log(`Hoje é dia ${dia} do mês ${mes} de ${ano}`);
-// Se hoje for 29 de Abril, a máquina vai imprimir: "Hoje é dia 29 do mês 3 de 2026".
+// Se hoje for 29 de Abril, a máquina vai imprimir: "Hoje é dia 29 do mês 4 de 2026".
 */
-let dataFormatura = new Date(2028, 11, 10);
-let dia = dataFormatura.getDate();
-let mes = dataFormatura.getMonth() +1;
-let ano = dataFormatura.getFullYear();
 
-console.log(`A formatura é ${dia} do mes ${mes} ano ${ano}`);
+// Definimos a data atual como o nosso ponto de partida.
+let dataDaProva = new Date();
 
+// Projetamos o agendamento da prova para daqui a 30 dias.
+dataDaProva.setDate(dataDaProva.getDate() + 30);
 
+// Verificamos em qual dia da semana essa nova data caiu. 
+//No JavaScript, 0 representa o Domingo e 6 representa o Sábado.
+let diaDaSemana = dataDaProva.getDay();
+
+// Iniciamos a regra de negócio: a prova não deve ocorrer no fim de semana.
+if(diaDaSemana === 6){
+    // Caso a data caia em um sábado, avançamos dois dias para que a prova ocorra na segunda-feira.
+    dataDaProva.setDate(dataDaProva.getDate() + 2);
+    
+} else if(diaDaSemana === 0){
+    // Caso caia em um domingo, avançamos apenas um dia, transferindo também para a segunda-feira.
+    dataDaProva.setDate(dataDaProva.getDate() + 1);
+}
+
+// Extraímos o dia e o mês para a apresentação final.
+let dia = dataDaProva.getDate();
+
+// Um detalhe técnico da linguagem: somamos 1 ao mês, pois o JavaScript os contabiliza de 0 (Janeiro) a 11 (Dezembro).
+let mes = dataDaProva.getMonth() + 1;
+
+// Por fim, exibimos o resultado do agendamento.
+console.log(`Data da prova: ${dia}/${mes}`);
+
+ 
